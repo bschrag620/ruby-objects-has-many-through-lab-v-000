8db4737c25ad736c4585ef4e1fe70941b4fd34cd
Brad Schrag <brad.schrag@gmail.com>
2018-11-21 04:08:22 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,13 @@
 class Genre
 
-  attr_accessor :name, :songs
+  attr_accessor :name, :songs, :artist
 
   @@all = []
 
   def initialize(name)
     self.name = name
     self.songs = []
+    self.artist = []
     @@all << self
   end
 

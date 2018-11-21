3f047bbc78a612c8497c9c06ac8817e10c674f2f
Brad Schrag <brad.schrag@gmail.com>
2018-11-21 03:48:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     self.name = name
+    self.songs = []
     @@all << self
   end
 
@@ -14,6 +15,7 @@ class Artist
   def new_song(name, genre)
     new_song = Song.new(name)
     new_song.artist = self
+    self.songs << new_song
     new_song.genre = genre
   end
 

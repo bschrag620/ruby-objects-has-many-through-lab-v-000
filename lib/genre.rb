07fe1a0f7bf11e3674c2f5623901923ee184bd41
Brad Schrag <brad.schrag@gmail.com>
2018-11-21 04:06:08 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,11 @@
 class Genre
 
-  attr_accessor :genre
+  attr_accessor :name
 
   @@all = []
 
-  def initialize(genre)
-    self.genre = genre
+  def initialize(name)
+    self.name = name
     @@all << self
   end
 end
